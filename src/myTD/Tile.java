ac720ee08fce5a3ea5bfe5d8100ab18c3e17fa5e
Pressler <r.pressler@snhu.edu>
2015-12-07 17:24:18 -0500

    Cleaned up Tile class

--- src/myTD/Tile.java
+++ src/myTD/Tile.java
@@ -3,8 +3,14 @@ package myTD;
 import java.awt.Color;
 
 public class Tile {
+	
+	//**********************Fields************************
+	
+	//Constants for representing tile types
 	public static final int GRASS = 0;
 	public static final int PATH = 1;
+	
+	//**********************Constructor************************
 	
 	public static Color getColor(int type) {
 		if(type == GRASS) {
