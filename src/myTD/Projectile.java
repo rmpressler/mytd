0aea2be3a6d373bdb4a0a285d348783f7b708d16
Pressler <r.pressler@snhu.edu>
2015-12-07 17:03:12 -0500

    Cleaned up Projectile class

--- src/myTD/Projectile.java
+++ src/myTD/Projectile.java
@@ -5,45 +5,40 @@ import java.awt.Graphics;
 
 public class Projectile {
 	
-	//location fields
-	private int x;
-	private int y;
-	private int r;
-	private int speed;
-	private int dmg;
+	//**********************Fields************************
 	
-	private boolean hit;
-	private boolean killed;
+	//Properties
+	private int x;				//x coordinate in pixels
+	private int y;				//y coordinate in pixels
+	private int r;				//radius in pixels
+	private int speed;			//speed in pixels per frame
+	private int dmg;			//damage done per hit
 	
-	Enemy target;
+	//State variables
+	private boolean hit;		//true when projectile hits [target]
+	private boolean killed;		//true when [target] is killed
+	
+	//Other references
+	Enemy target;				//Reference to enemy being targeted
+	
+	//**********************Constructor************************
 	
 	public Projectile(int newX, int newY, int newDmg, Enemy newTarget) {
+		
+		//init
 		x = newX;
 		y = newY;
-		dmg = newDmg;
-		target = newTarget;
-		speed = 5;
 		r = 5;
+		speed = 5;
+		dmg = newDmg;
+		
 		hit = false;
+		killed = false;
+		
+		target = newTarget;
 	}
 	
-	public Enemy getTarget() {
-		return target;
-	}
-	
-	public boolean killed() {
-		return killed;
-	}
-	
-	public boolean checkCollision() {
-		if(x >= target.getX() &&
-				x <= target.getX() + target.getSize() &&
-				y >= target.getY() &&
-				y <= target.getY() + target.getSize()) {
-			return true;
-		}
-		else {return false;}
-	}
+	//**********************Update and draw************************
 	
 	public void update() {
 		if(x < target.getX()) {
@@ -62,13 +57,8 @@ public class Projectile {
 		
 		if(checkCollision()) {
 			killed = target.hit(dmg);
-			
 			hit = true;
 		}
-	}
-	
-	public boolean getHit() {
-		return hit;
 	}
 	
 	public void draw(Graphics g) {
@@ -76,5 +66,31 @@ public class Projectile {
 		g.fillOval(x,  y,  r * 2,  r * 2);
 		g.setColor(Color.yellow.darker());
 		g.drawOval(x,  y, r * 2, r * 2);
+	}
+	
+	//**********************Getters************************
+	
+	public boolean getHit() {
+		return hit;
+	}
+	
+	public Enemy getTarget() {
+		return target;
+	}
+	
+	//**********************Game methods************************
+	
+	public boolean killed() {
+		return killed;
+	}
+	
+	public boolean checkCollision() {
+		if(x >= target.getX() &&
+				x <= target.getX() + target.getSize() &&
+				y >= target.getY() &&
+				y <= target.getY() + target.getSize()) {
+			return true;
+		}
+		else {return false;}
 	}
 }
